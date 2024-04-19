3ef04c6728a52e984dcac8e5a609be938e16b860
brukyu <bbrunoigorr@gmail.com>
2024-04-19 09:39:13 -0300

    add taskcompleted screen

---
+++ styles/CompletedTasksScreenStyles.js
@@ -0,0 +1,11 @@
+// styles/CompletedTasksScreenStyles.js
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#000',
+    color:'white'
+  },
+});
